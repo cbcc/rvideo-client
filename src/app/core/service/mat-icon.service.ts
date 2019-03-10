dedc55fa27220f6f5176639425ae784c5f15ad97
cbc <827718623@qq.com>
2019-03-10 19:30:56 +0800

    feat(core): add more icons

--- src/app/core/service/mat-icon.service.ts
+++ src/app/core/service/mat-icon.service.ts
@@ -16,5 +16,20 @@ export class MatIconService {
     this.iconRegistry.addSvgIcon(
       'view-module',
       this.sanitizer.bypassSecurityTrustResourceUrl('assets/icon/baseline-view_module-24px.svg'));
+    this.iconRegistry.addSvgIcon(
+      'home',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icon/baseline-home-24px.svg'));
+    this.iconRegistry.addSvgIcon(
+      'unarchive',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icon/baseline-unarchive-24px.svg'));
+    this.iconRegistry.addSvgIcon(
+      'favorite',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icon/baseline-favorite-24px.svg'));
+    this.iconRegistry.addSvgIcon(
+      'bookmarks',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icon/baseline-bookmarks-24px.svg'));
+    this.iconRegistry.addSvgIcon(
+      'account-box',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icon/baseline-account_box-24px.svg'));
   }
 }
